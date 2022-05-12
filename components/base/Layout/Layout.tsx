f0c2c8f1e01ac9b34e4ad916d5c20b8c1d7c99ed
Victor SALOMON <victor-salomon@hotmail.fr>
2022-05-12 15:13:20 +0200

    TFT-9 update desktop footer

--- components/base/Layout/Layout.tsx
+++ components/base/Layout/Layout.tsx
@@ -24,7 +24,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
       </header>
       {process.env.NEXT_PUBLIC_GA && process.env.NODE_ENV === 'production' && <Analytics />}
       {children}
-      <Footer />
+      <Footer isSocials={true} isCredentialCustoms={true} isTernoaOfficial={true} projectName="Toolkit" />
     </>
   )
 }
